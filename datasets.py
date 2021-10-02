34b64a5f0d862e9c8916be34fd8c995b958a168e
ordogalen <ordogalen@gmail.com>
2021-10-02 18:34:06 +0200

    first commit

---
+++ datasets.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 30 19:09:44 2020
+
+@author: krishna
+"""
+
+import os
+import numpy as np
+import glob
+import argparse
+
+class_ids = {'English': 0, 'Hindi': 1, 'Kannada': 2, 'Tamil': 3, 'Telugu': 4, 'Malayalam': 5, 'Marathi': 6,
+             'Gujarathi': 7}
+
+
+def create_meta(files_list, store_loc, mode='train'):
+    if not os.path.exists(store_loc):
+        os.makedirs(store_loc)
+
+    if mode == 'train':
+        meta_store = store_loc + '/training.txt'
+        fid = open(meta_store, 'w')
+        for filepath in files_list:
+            fid.write(filepath + '\n')
+        fid.close()
+    elif mode == 'test':
+        meta_store = store_loc + '/testing.txt'
+        fid = open(meta_store, 'w')
+        for filepath in files_list:
+            fid.write(filepath + '\n')
+        fid.close()
+    elif mode == 'validation':
+        meta_store = store_loc + '/validation.txt'
+        fid = open(meta_store, 'w')
+        for filepath in files_list:
+            fid.write(filepath + '\n')
+        fid.close()
+    else:
+        print('Error in creating meta files')
+
+
+def extract_files(folder_path):
+    all_lang_folders = sorted(glob.glob(folder_path + '/*/'))
+    train_lists = []
+    test_lists = []
+    val_lists = []
+
+    train_nums = len(all_lang_folders) - int(len(all_lang_folders) * 0.15) - int(len(all_lang_folders) * 0.05)
+
+    for i in range(train_nums):
+        speaker_id = all_lang_folders[i].split('\\')[1]
+        all_files = sorted(glob.glob(all_lang_folders[i]+'\*'))
+        for audio_filepath in all_files:
+            to_write = audio_filepath + ' ' + str(speaker_id)
+            train_lists.append(to_write)
+
+    for i in range(train_nums, train_nums + int(len(all_lang_folders) * 0.10)):
+        speaker_id = all_lang_folders[i].split('\\')[1]
+        all_files = sorted(glob.glob(all_lang_folders[i]+'\*'))
+        for audio_filepath in all_files:
+            to_write = audio_filepath + ' ' + str(speaker_id)
+            val_lists.append(to_write)
+
+    for i in range(train_nums + int(len(all_lang_folders) * 0.05), len(all_lang_folders)):
+        speaker_id = all_lang_folders[i].split('\\')[1]
+        all_files = sorted(glob.glob(all_lang_folders[i]+'\*'))
+        for audio_filepath in all_files:
+            to_write = audio_filepath + ' ' + str(speaker_id)
+            test_lists.append(to_write)
+
+    return train_lists, test_lists, val_lists
+
+
+if __name__ == '__main__':
+    #parser = argparse.ArgumentParser("Configuration for data preparation")
+    #parser.add_argument("--processed_data", default="/media/newhd/youtube_lid_data/download_data", type=str,
+    #                    help='Dataset path')
+    #parser.add_argument("--meta_store_path", default="meta/", type=str, help='Save directory after processing')
+    #config = parser.parse_args()
+    #train_list, test_list, val_lists = extract_files(config.processed_data)
+
+    parser = argparse.ArgumentParser("Configuration for data preparation")
+    parser.add_argument("--processed_data", default="meta/speakers", type=str,
+                        help='Dataset path')
+    parser.add_argument("--meta_store_path", default="meta/", type=str, help='Save directory after processing')
+    config = parser.parse_args()
+    train_list, test_list, val_lists = extract_files(config.processed_data)
+    create_meta(train_list, config.meta_store_path, mode='train')
+    create_meta(test_list, config.meta_store_path, mode='test')
+    create_meta(val_lists, config.meta_store_path, mode='validation')
+
---
+++ feature_extraction.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 31 11:15:47 2020
+@author: krishna
+"""
+
+import os
+import numpy as np
+from utils import utils
+
+
+def extract_features(audio_filepath):
+    features = utils.feature_extraction(audio_filepath)
+    return features
+
+
+def FE_pipeline(feature_list, store_loc, mode):
+    global fid
+    create_root = os.path.join(store_loc, mode)
+    if not os.path.exists(create_root):
+        os.makedirs(create_root)
+    if mode == 'train':
+        fid = open('meta/training_feat.txt', 'w')
+    elif mode == 'test':
+        fid = open('meta/testing_feat.txt', 'w')
+    elif mode == 'validation':
+        fid = open('meta/validation_feat.txt', 'w')
+    else:
+        print('Unknown mode')
+
+    for row in feature_list:
+        filepath = row.split(' ')[0]
+        speaker_id = row.split(' ')[1]
+        filename = filepath.split('\\')[2]
+
+        create_folders = os.path.join(create_root)
+        if not os.path.exists(create_folders):
+            os.makedirs(create_folders)
+        extract_feats = extract_features(filepath)
+        dest_filepath = create_folders + '/' + filename[:-4] + '.npy'
+        np.save(dest_filepath, extract_feats)
+        to_write = dest_filepath + ' ' + speaker_id
+        fid.write(to_write + '\n')
+    fid.close()
+
+
+if __name__ == '__main__':
+    store_loc = 'meta/Features'
+    read_train = [line.rstrip('\n') for line in open('meta/training.txt')]
+    FE_pipeline(read_train, store_loc, mode='train')
+
+    read_test = [line.rstrip('\n') for line in open('meta/testing.txt')]
+    FE_pipeline(read_test, store_loc, mode='test')
+
+    read_val = [line.rstrip('\n') for line in open('meta/validation.txt')]
+    FE_pipeline(read_val, store_loc, mode='validation')
+
