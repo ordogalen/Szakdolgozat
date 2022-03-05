bd5a42fd3c433c2af3c6564648cc91a4b75a86d0
ordogalen <ordogalen@gmail.com>
2022-03-05 20:46:03 +0100

    Added plotting, improve training and validating

--- training_xvector.py
+++ training_xvector.py
@@ -18,6 +18,7 @@ from sklearn.metrics import accuracy_score
 from utils.utils import speech_collate
 import matplotlib.pyplot as plt
 
+
 def draw_curve(current_epoch):
     x_epoch.append(current_epoch)
     ax0.plot(x_epoch, y_loss['train'], 'bo-', label='train')
@@ -42,47 +43,38 @@ ax1 = fig.add_subplot(122, title="mean accuracy")
 
 # Argument parser
 parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument('-training_filepath', type=str, default='meta/bea/speakers')
-parser.add_argument('-testing_filepath', type=str, default='meta/bea/speakers')
-parser.add_argument('-validation_filepath', type=str, default='meta/bea/speakers')
-
 parser.add_argument('-audio_files', type=str, default='meta/bea/speakers')
-
-parser.add_argument('-input_dim', action="store_true", default=120)
-parser.add_argument('-num_classes', action="store_true", default=89)
+parser.add_argument('-input_dim', action="store_true", default=257)
+parser.add_argument('-num_classes', action="store_true", default=12)
 parser.add_argument('-lamda_val', action="store_true", default=0.1)
 parser.add_argument('-batch_size', action="store_true", default=64)
 parser.add_argument('-use_gpu', action="store_true", default=True)
-parser.add_argument('-num_epochs', action="store_true", default=12)
+parser.add_argument('-num_epochs', action="store_true", default=60)
 args = parser.parse_args()
 
 # Data related
-SHUFFLE_SEED = 45  # random seed
-dataset = SpeechDataGenerator(dataset_audio_path=args.audio_files, mode='train', shuffle_seed=SHUFFLE_SEED)
+dataset = SpeechDataGenerator(dataset_audio_path=args.audio_files, mode='train')
 
 args.num_classes = len(set(dataset.labels))
-
-train_size = int(0.80 * len(dataset))
+train_size = int(0.85 * len(dataset))
 test_size = len(dataset) - train_size
 train_dataset, test_dataset = torch.utils.data.random_split(dataset, [train_size, test_size])
+
 
 print("All data:" + str(len(dataset)))
 print("Training on " + str(len(train_dataset)))
 print("Validating on " + str(len(test_dataset)))
 
-dataloader_train = DataLoader(train_dataset, batch_size=args.batch_size, collate_fn=speech_collate, pin_memory=False)
-dataloader_val = DataLoader(test_dataset, batch_size=args.batch_size, collate_fn=speech_collate, pin_memory=False)
+dataloader_train = DataLoader(train_dataset, batch_size=args.batch_size, collate_fn=speech_collate, pin_memory=False, shuffle=True )
+dataloader_val = DataLoader(test_dataset, batch_size=args.batch_size, collate_fn=speech_collate, pin_memory=False, shuffle=True)
 
-# dataset_test = SpeechDataGenerator(dataset_audio_path=args.training_filepath, mode='test', shuffle_seed=SHUFFLE_SEED)
-# dataloader_test = DataLoader(dataset_test, batch_size=args.batch_size, shuffle=True, collate_fn=speech_collate)
 
 # Model related
 use_cuda = True  # torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
 model = X_vector(args.input_dim, args.num_classes).to(device)
-optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0, betas=(0.9, 0.999), eps=1e-8)
+optimizer = optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0, betas=(0.9, 0.98), eps=1e-9)
 loss_fun = nn.CrossEntropyLoss()
-
 
 def train(dataloader_train, epoch):
     train_loss_list = []
@@ -101,6 +93,7 @@ def train(dataloader_train, epoch):
         loss = loss_fun(pred_logits, labels.long())
         loss.backward()
         optimizer.step()
+
         train_loss_list.append(loss.item())
         if i_batch % 10 == 0:
             print('Loss {} after {} iteration'.format(np.mean(np.asarray(train_loss_list)), i_batch))
@@ -154,9 +147,8 @@ def validation(dataloader_val, epoch):
 
 
 if __name__ == '__main__':
-    if not os.path.exists("meta/FeatureExt"):
-        os.makedirs("meta/FeatureExt")
-
     for epoch in range(args.num_epochs):
         train(dataloader_train, epoch)
         validation(dataloader_val, epoch)
+        draw_curve(epoch)
+
