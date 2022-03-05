29a5430a727390731b93c8f85b079e94955d7016
ordogalen <ordogalen@gmail.com>
2022-03-05 20:34:16 +0100

    Added comments

--- feature_extractor.py
+++ feature_extractor.py
@@ -5,64 +5,73 @@ import torch
 from models.x_vector_Indian_LID import X_vector
 from utils import utils
 
-#PATH = "./save_model/best_check_point_15_0.3372245247165362"
-PATH = "save_model_2/best_check_point_29_1.2584018047819747"
-
-net = X_vector(120, 89)
+# PATH = "./save_model/best_check_point_15_0.3372245247165362"
+PATH = "save_model/best_check_point_55_0.8169845943339169"
+PATH_B = "save_model/best_check_point_32_0.7193116955459118"
+wav_aibo = "./meta/aibo/wav-aibo/"
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
-model = X_vector(120, 89).to(device)
+model = X_vector(257, 12).to(device)
 checkpoint = torch.load(os.path.join(PATH))
 model.load_state_dict(checkpoint['model'])
 layer = model.modules.get('segment6')
 
 
-def get_vector(raw_wav):
-    spec = utils.load_data(raw_wav, mode="train")
-    sample = {'features': torch.from_numpy(np.ascontiguousarray(spec)),}
+def get_vector(raw_wav, mode):
+    """
+    :param raw_wav: A single audio wav files
+    :param mode: 'train' or 'test', it is needed to determine how the wav should be process
+    :return: the feature x-vector which is (1, 512) size
+    """
+    spec = utils.load_data(raw_wav, mode=mode)
+    sample = {'features': torch.from_numpy(np.ascontiguousarray(spec)), }
 
     model.eval()
-    my_embedding = torch.zeros(1,512)
+    my_embedding = torch.zeros(1, 512)
+
     def copy_data(m, i, o):
         my_embedding.copy_(o.data)
+
     h = layer.register_forward_hook(copy_data)
 
     features = torch.from_numpy(np.asarray([sample['features'].numpy().T]))
     model(features.to(device))
+
     h.remove()
     return my_embedding
 
-wav_aibo = "./meta/aibo/wav-aibo/"
 
-def extract_features_into_file(raw, label, mode):
+def extract_features_into_file(raw, label, mode, spec_mode):
+    """
+    :param raw: The '*.txt' file which contains the audio path's
+    :param label: The '*.txt' file which contains the label for the audio's
+    :param mode: 'train' or 'test' or 'dev' for making the .npy files into these directories
+    :param spec_mode: 'train' or 'test', it is needed to determine how the wav should be process
+    """
     filesDone = 0
     files = open(raw, "r")
     labels = open(label, "r")
     dir = ""
     if (mode == 'train'):
-        dir = "./meta/aibo/train_downsample2/"
-    if(mode == "dev"):
-        dir = "./meta/aibo/dev2/"
-    if(mode == "test"):
-        dir = "./meta/aibo/test2/"
+        dir = "./meta/aibo/train_downsample/"
+    if (mode == "dev"):
+        dir = "./meta/aibo/dev/"
+    if (mode == "test"):
+        dir = "./meta/aibo/test/"
     for i, (key1, key2) in enumerate(zip(files, labels)):
-        feature = get_vector(wav_aibo+key1.strip()+".wav")
+        feature = get_vector(wav_aibo + key1.strip() + ".wav", spec_mode)
 
-        np.save(dir+key1.strip()+"_#"+key2.strip(), feature)
+        np.save(dir + key1.strip() + "_#" + key2.strip(), feature)
         filesDone += 1
-        if(filesDone % 100 == 0):
+        if (filesDone % 100 == 0):
             print(filesDone)
 
     files.close()
     labels.close()
 
 
-extract_features_into_file("./meta/aibo/filelist.raw.test.txt","./meta/aibo/labels.test.txt","test")
-extract_features_into_file("./meta/aibo/filelist.raw.dev.txt","./meta/aibo/labels.dev.txt","dev")
-extract_features_into_file("./meta/aibo/filelist.raw.train.downsample.txt","./meta/aibo/labels.train.downsample.txt","train")
-
-
-
-
-
+extract_features_into_file("./meta/aibo/filelist.raw.test.txt", "./meta/aibo/labels.test.txt", "test", "test")
+extract_features_into_file("./meta/aibo/filelist.raw.dev.txt", "./meta/aibo/labels.dev.txt", "dev", "train")
+extract_features_into_file("./meta/aibo/filelist.raw.train.downsample.txt", "./meta/aibo/labels.train.downsample.txt",
+                           "train", "train")
